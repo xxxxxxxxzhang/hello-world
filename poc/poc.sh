cc8779cac923f671cf7bba7ae5e73066857d1817
zzxin107 <972844082@qq.com>
2020-05-25 16:02:26 +0800

    修改脚本

--- poc/poc.sh
+++ poc/poc.sh
@@ -2,10 +2,7 @@ tar -xvzf /opt/poc/geckodriver-v0.26.0-linux64.tar.gz
 chmod +x geckodriver 
 cp geckodriver /usr/local/bin/ 
 echo "start"
-firefox --version
 sleep 10
-echo "http://192.168.56.107:8000:"
-curl http://192.168.56.107:8000
 echo "http://web:"
 curl http://web
 python3 /opt/poc/exp.py http://web
