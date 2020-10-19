d2b421a69ccd894ce27a1c0f16743ff6c6994cd6
zhangzhixin <972844082@qq.com>
2020-10-19 15:22:37 +0800

    修改run.sh文件

--- web/run.sh
+++ web/run.sh
@@ -16,19 +16,19 @@ mkdir -p /var/run/php/
 chown nobody:nobody /var/run/php/
 
 # 更新源、安装openssh 并修改配置文件和生成key 并且同步时间
-#echo "http://mirrors.aliyun.com/alpine/latest-stable/main/" > /etc/apk/repositories
-#echo "http://mirrors.aliyun.com/alpine/latest-stable/community/" >> /etc/apk/repositories
-#apk update 
-#apk add --no-cache openssh-server tzdata 
-#cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime 
-#sed -i "s/#PermitRootLogin.*/PermitRootLogin yes/g" /etc/ssh/sshd_config 
-#ssh-keygen -t rsa -P "" -f /etc/ssh/ssh_host_rsa_key 
-#ssh-keygen -t ecdsa -P "" -f /etc/ssh/ssh_host_ecdsa_key 
-#ssh-keygen -t ed25519 -P "" -f /etc/ssh/ssh_host_ed25519_key 
-#echo "root:admin" | chpasswd
-#/usr/sbin/sshd
+echo "http://mirrors.aliyun.com/alpine/latest-stable/main/" > /etc/apk/repositories
+echo "http://mirrors.aliyun.com/alpine/latest-stable/community/" >> /etc/apk/repositories
+apk update 
+apk add --no-cache openssh-server tzdata -y
+cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime 
+sed -i "s/#PermitRootLogin.*/PermitRootLogin yes/g" /etc/ssh/sshd_config 
+ssh-keygen -t rsa -P "" -f /etc/ssh/ssh_host_rsa_key 
+ssh-keygen -t ecdsa -P "" -f /etc/ssh/ssh_host_ecdsa_key 
+ssh-keygen -t ed25519 -P "" -f /etc/ssh/ssh_host_ed25519_key 
+echo "root:admin" | chpasswd
+/usr/sbin/sshd
 
-#touch /var/log/php-fpm.log
+touch /var/log/php-fpm.log
 chown nobody:nobody /var/log/php-fpm.log
 
 
