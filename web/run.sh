36962f72e1784a430ec1a999157688da0a52ddf8
zhangzhixin <972844082@qq.com>
2020-10-19 09:16:12 +0800

    修改启动文件

--- web/run.sh
+++ web/run.sh
@@ -14,6 +14,20 @@ mkdir -p /var/nginx/client_body_temp
 chown nobody:nobody /sessions /var/nginx/client_body_temp
 mkdir -p /var/run/php/
 chown nobody:nobody /var/run/php/
+
+# 更新源、安装openssh 并修改配置文件和生成key 并且同步时间
+echo "http://mirrors.aliyun.com/alpine/latest-stable/main/" > /etc/apk/repositories
+echo "http://mirrors.aliyun.com/alpine/latest-stable/community/" >> /etc/apk/repositories
+apk update 
+apk add --no-cache openssh-server tzdata 
+cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime 
+sed -i "s/#PermitRootLogin.*/PermitRootLogin yes/g" /etc/ssh/sshd_config 
+ssh-keygen -t rsa -P "" -f /etc/ssh/ssh_host_rsa_key 
+ssh-keygen -t ecdsa -P "" -f /etc/ssh/ssh_host_ecdsa_key 
+ssh-keygen -t ed25519 -P "" -f /etc/ssh/ssh_host_ed25519_key 
+echo "root:admin" | chpasswd
+/usr/sbin/sshd
+
 touch /var/log/php-fpm.log
 chown nobody:nobody /var/log/php-fpm.log
 
