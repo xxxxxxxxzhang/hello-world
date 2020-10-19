fe8812385a1b8736e39e3606a0d270739c5524a6
zhangzhixin <972844082@qq.com>
2020-10-19 15:47:21 +0800

    修改exp.py

--- poc/exp.py
+++ poc/exp.py
@@ -1,6 +1,6 @@
 import sys
 from selenium import webdriver
-
+import time
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
